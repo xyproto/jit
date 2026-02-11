4d3af32ee00c5efa00b939ccfb0673baab14b308
xnacly <47723417+xNaCly@users.noreply.github.com>
2026-02-11 14:12:24 +0100

    jit_test: remove constant test

--- jit_test.go
+++ jit_test.go
@@ -1,7 +1,6 @@
 package jit
 
 import (
-	"fmt"
 	"runtime"
 	"testing"
 )
@@ -21,7 +20,6 @@ func TestSquare(t *testing.T) {
 	if err != nil {
 		t.Fatalf("jit failed: %v", err)
 	}
-	fmt.Printf("%p\n", function)
 
 	expected := int32(64 * 64)
 	got := function(64)
@@ -29,46 +27,4 @@ func TestSquare(t *testing.T) {
 		t.Fatalf("got(%v) != expected(%v)", got, expected)
 	}
 	j.Destroy()
-}
-
-func TestExecute_ReturnConstant(t *testing.T) {
-	if runtime.GOARCH != "amd64" {
-		t.Skip("amd64 only")
-	}
-
-	tests := []struct {
-		name string
-		val  int32
-	}{
-		{"zero", 0},
-		{"small", 21},
-		{"large", 123456789},
-		{"negative", -42},
-		{"max", 0x7fffffff},
-		{"min", -0x80000000},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			code := make([]byte, 6)
-
-			// mov eax, imm32
-			code[0] = 0xB8
-
-			// ret
-			code[5] = 0xC3
-
-			j := Jit[int32, int32]{}
-			function, err := j.NewFunc(code)
-			if err != nil {
-				t.Fatalf("jit failed: %v", err)
-			}
-
-			got := function(tt.val)
-			j.Destroy()
-			if int32(got) != tt.val {
-				t.Fatalf("wrong return: got %d, want %d", got, tt.val)
-			}
-		})
-	}
 }
